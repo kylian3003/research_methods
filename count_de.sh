7ce8c672d40ec80c3313d85c8245aef963889a47
Kylian de Rooij <kylianderooij@gmail.com>
2021-03-10 01:43:09 +0100

    Modified: count_de.sh -Added comments explaining what each part of the program does and why it is necessary. (as these changes did not commit last time)

--- count_de.sh
+++ count_de.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
+# Program asks user to input file name (any text file (.txt) works
 echo "Enter a file name:"
+
+# Program reads the file name and gives it a variable 'fname'
 read fname
 if [ -z "$fname" ]
 then
 	exit
 fi
 
+# Echo statement for nice structure
 echo "Number of occurrences of the word 'de' in $fname:"
-cat $fname | grep -iow de |wc -l
+
+# Program counts the number of occurrences of 'de' in the given text file
+# -i  makes the search case insensitive, meaning that all occurrences of
+# 'de' will be shown, no matter the capitalization
+# -o makes it so it only looks for the word, not the line that it is in
+# -w makes the search word based, so that 'de' will only be counted if
+# if is a word of its own, so not when it's part of another word such as
+# 'NeDErlands'.
+cat $fname | grep -i -o -w de |wc -l
